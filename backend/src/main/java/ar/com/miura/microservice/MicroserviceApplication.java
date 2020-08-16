2b803591031cf4d18d8a657a2f10c204706c8a86
Agustin Miura <agustin.miura@gmail.com>
2020-08-16 18:38:35 -0300

    First commit

---
+++ backend/src/main/java/ar/com/miura/microservice/MicroserviceApplication.java
@@ -0,0 +1,13 @@
+package ar.com.miura.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroserviceApplication.class, args);
+	}
+
+}
