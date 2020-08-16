a0985cd1ab8d339c8437e306c14b60f50c09dacc
Agustin Miura <agustin.miura@gmail.com>
2020-08-16 19:16:02 -0300

    Added controller sample

--- backend/src/main/java/ar/com/miura/application/Application.java
+++ backend/src/main/java/ar/com/miura/application/Application.java
@@ -1,13 +1,13 @@
-package ar.com.miura.microservice;
+package ar.com.miura.application;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MicroserviceApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MicroserviceApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
