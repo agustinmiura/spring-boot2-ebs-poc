6d6c918948a507fa4b061138e5b7a931991719e0
Agustin Miura <agustin.miura@gmail.com>
2020-08-16 19:56:03 -0300

    Added sql

---
+++ sql/person.sql
@@ -0,0 +1,11 @@
+-- public.person definition
+
+-- Drop table
+
+-- DROP TABLE public.person;
+
+CREATE TABLE public.person (
+	id int8 NOT NULL,
+	"name" varchar NULL,
+	surname varchar NULL
+);
---
+++ sql/sample_data.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.person
+(id, "name", surname)
+VALUES(4, '4', '4');
+INSERT INTO public.person
+(id, "name", surname)
+VALUES(5, '5', '5');
